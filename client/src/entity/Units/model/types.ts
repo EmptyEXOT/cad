1fdf8c27090be77648f4f085c05ea62e70daa87f
Alim Sadullaev <idbeholdv@MacBook-Pro-Alim.local>
2024-08-17 17:34:48 +0300

    unit types and defaults

--- client/src/entity/Units/model/types.ts
+++ client/src/entity/Units/model/types.ts
@@ -10,3 +10,35 @@ export type UnitModel = {
     position: XYPosition,
     handles: NodeHandle[]
 }
+
+export type UnitBase = {
+    // TODO add enum or string union type for safetyLevel 
+    safetyLevel: string,
+    // TODO add enum or string union type for explosionProtection 
+    explosionProtection: string,
+    // TODO add enum or string union type for fireProteciton 
+    fireProteciton: string,
+    ip: IPLevel,
+}
+
+export interface MinMax<L = number, H = number>  {
+    min: L,
+    max: H
+}
+
+export interface IPLevel {
+    dust: 0|1|2|3|4|5|6|7|8|9,
+    water: 0|1|2|3|4|5|6|7|8|9,
+}
+
+export enum AgitatorType {
+    Paddle = 'paddle',
+    Anchor = 'anchor',
+    Turbine = 'turbine',
+}
+
+export interface Agitator {
+    type: AgitatorType,
+    drivePower: number,
+    speed: number,
+}
